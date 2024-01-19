7b7faf50ed13a3eed9bd9721b9678676d1fa03c9
Divyavenkatesan-max <vdivyabharathi68@gmail.com>
2024-01-19 10:58:42 +0530

    new update

--- src/test/java/test/maintest.java
+++ src/test/java/test/maintest.java
@@ -1,5 +1,13 @@
 package test;
 
 public class maintest {
+	public static void main(String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		
+		
+	}
 
 }
+
